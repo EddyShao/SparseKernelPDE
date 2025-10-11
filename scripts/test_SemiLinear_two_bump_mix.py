166aad482a009d6cd29d9b43d22a7ec62c21a500
Zihan Shao <eddyszh@outlook.com>
2025-10-11 10:58:16 -0700

    update mix_solver

--- scripts/test_SemiLinear_two_bump_mix.py
+++ scripts/test_SemiLinear_two_bump_mix.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append("./")
 from pde.SemiLinearPDE import PDE
-from src.utils import Objective, compute_errors, compute_y, compute_rhs
+from src.utils import Objective, compute_errors, compute_y, compute_rhs, merge_kernel_cluster
 import jax.numpy as jnp
 
 
@@ -20,10 +20,10 @@ parser = argparse.ArgumentParser(description='Run the algorithm to solve PDE pro
 parser.add_argument('--anisotropic', action='store_true', help='Enable anisotropic mode (default: False)')
 parser.add_argument('--sigma_max', type=float, default=1.0, help='Maximum value of the kernel width.')
 parser.add_argument('--sigma_min', type=float, default=1e-3, help='Minimum value of the kernel width.')
-parser.add_argument('--blocksize', type=int, default=100, help='Block size for the anisotropic mode.')
+parser.add_argument('--blocksize', type=int, default=300, help='Block size for the anisotropic mode.')
 parser.add_argument('--Nobs', type=int, default=50, help='Base number of observations')
 parser.add_argument('--sampling', type=str, default='grid', help='Sampling method for the observations.')
-parser.add_argument('--scale', type=float, default=1000, help='penalty for the boundary condition')
+parser.add_argument('--scale', type=float, default=0, help='penalty for the boundary condition')
 parser.add_argument('--TOL', type=float, default=1e-5, help='Tolerance for stopping.')
 parser.add_argument('--max_step', type=int, default=5000, help='Maximum number of steps.')
 parser.add_argument('--print_every', type=int, default=100, help='Print every n steps.')
@@ -51,22 +51,67 @@ if 'newton' in alg_opts['solver']:
     from src.solver import solve
 elif 'first' in alg_opts['solver']:
     from src.solver_first_order import solve
+    from src.solver import solve as solve_second_order
+    alg_opts['max_step'] = 500
 else:
     raise ValueError('Unknown solver type.')
 
+# comment out if you want to use the smooth transition function
+def ex_sol_help(x, center=(0.30, 0.30), k=8, R_0=0.2):
+    x = jnp.atleast_2d(x)  # Ensures x has shape (N, 2)
+    R = jnp.sqrt((x[:, 0] - center[0])**2 + (x[:, 1] - center[1])**2)
+    return jnp.tanh(k * (R_0 - R)) + 1
+
+def f_help(x, center=(0.2, 0.30), k=8, R_0=0.2):
+    x = jnp.atleast_2d(x)  # Ensures x has shape (N, 2)
+    R = jnp.sqrt((x[:, 0] - center[0])**2 + (x[:, 1] - center[1])**2)
+    tanh_term = jnp.tanh(k * (R_0 - R))
+    tanh_sq = tanh_term**2
+    term_x = (-2 * k * (x[:, 0] - center[0])**2 * tanh_term / R**2) + ((x[:, 0] - center[0])**2 / R**3) - (1 / R)
+    term_y = (-2 * k * (x[:, 1] - center[1])**2 * tanh_term / R**2) + ((x[:, 1] - center[1])**2 / R**3) - (1 / R)
+    result = k * (tanh_sq - 1) * (term_x + term_y)
+    return result
+
+#########################################################
+####################### two bump ########################
+#########################################################
+
+R_1 = 0.3
+R_2 = 0.15
+center_1 = [0.30, 0.30]
+center_2 = [-0.30, -0.30]
+k1 = 12
+k2 = 4
+
+def ex_sol(x):
+    return ex_sol_help(x, center=center_1, k=k1, R_0=R_1) + ex_sol_help(x, center=center_2, k=k2, R_0=R_2)
+
+
+def f(x):
+    return f_help(x, center=center_1, k=k1, R_0=R_1) + f_help(x, center=center_2, k=k2, R_0=R_2) + ex_sol(x) ** 3
+
 
 p = PDE(alg_opts)
-p.name = 'SemiLinearSines'
+
+p.f = f
+p.ex_sol = ex_sol
+p.name = 'SemiLinearTwoBumpAdaptive'
+p.kernel.pad_size = 300
+
+
 rhs = p.f(p.xhat)
 
 # optional: add noise to the rhs
 if args.add_noise:
-    rhs_mag = np.max(np.abs(rhs[:-p.Nx_bnd]))
-    noise = np.random.randn(p.Nx) * 0.01 * rhs_mag
+    rhs_mag = jnp.max(jnp.abs(rhs[:-p.Nx_bnd]))
+    noise = jnp.random.randn(p.Nx) * 0.01 * rhs_mag
     rhs += noise
-
+# rhs[-p.Nx_bnd:] = p.ex_sol(p.xhat_bnd)
 rhs = rhs.at[-p.Nx_bnd:].set(p.ex_sol(p.xhat_bnd))
 
+rhs_test_int = p.f(p.test_int)
+rhs_test_bnd = p.ex_sol(p.test_bnd)
+rhs_test = jnp.concatenate((rhs_test_int, rhs_test_bnd))
 
 
 def evaluate_and_save_solution(p, rhs, alg_opts, args):
@@ -86,6 +131,28 @@ def evaluate_and_save_solution(p, rhs, alg_opts, args):
     print('#' * 20)
     print()
     alg_out = solve(p, rhs, alg_opts)
+
+    if 'first' in alg_opts['solver']:
+        xk  = alg_out['xk'][-1]
+        sk  = alg_out['sk'][-1]
+        ck  = alg_out['ck'][-1]
+        suppc = alg_out['suppc'][-1]
+        xk, sk, ck = merge_kernel_cluster(suppc, xk, sk, ck, p, tau=1.0)
+        # pad to pad_size
+        ck = jnp.pad(ck, (0, p.kernel.pad_size - len(ck)), constant_values=0.)
+        sk = jnp.pad(sk, ((0, p.kernel.pad_size - sk.shape[0]), (0, 0)), constant_values=0.)
+        xk = jnp.pad(xk, ((0, p.kernel.pad_size - xk.shape[0]), (0, 0)), constant_values=0.)
+        # use second order solver to refine the solution
+        alg_opts_second = alg_opts.copy()
+        alg_opts_second['solver'] = 'newton'
+        p.u_zero = {
+            'x': jnp.array(xk),
+            's': jnp.array(sk),
+            'u': jnp.array(ck)
+        }
+        alg_out = solve_second_order(p, rhs, alg_opts_second)
+
+
 
     rhs_test = np.concatenate((p.f(p.test_int), p.ex_sol(p.test_bnd)))
     p.obj_test = Objective(p.test_int.shape[0], p.test_bnd.shape[0], scale=alg_opts['scale'])
@@ -172,4 +239,17 @@ def evaluate_and_save_solution(p, rhs, alg_opts, args):
 
     return alg_out
 
-alg_out = evaluate_and_save_solution(p, rhs, alg_opts, args)
+
+for _ in range(3):
+    # Solve the PDE and evaluate the solution
+    alg_out = evaluate_and_save_solution(p, rhs, alg_opts, args)
+    suppc = alg_out['suppc'][-1]
+    p.u_zero = {
+        'x': jnp.array(alg_out['xk'][-1, :, :] * suppc[:, None]),
+        's': jnp.array(alg_out['sk'][-1, :, :] * suppc[:, None]),
+        'u': jnp.array(alg_out['ck'][-1, :] * suppc)
+    }
+    
+    alg_opts['alpha'] = 0.1 * alg_opts['alpha']  
+    alg_opts['T'] = 10 * alg_opts['T']
+    alg_opts['max_step'] = 2000
