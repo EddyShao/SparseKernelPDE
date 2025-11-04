a645b569d6efbc60b41bac4ecc98fbb1267ecd3d
Zihan Shao <eddyszh@outlook.com>
2025-11-04 11:12:44 -0800

    bugs update for high-dim Semilinear

--- pde/SemiLinearHighDim.py
+++ pde/SemiLinearHighDim.py
@@ -176,7 +176,10 @@ class PDE:
         method: 'uniform' or 'grid'
         """
 
-        return sample_cube_obs(self.D, Nobs, method=method)
+        # obs_int, obs_bnd = sample_cube_obs(self.D, Nobs, method=method)
+        Nobs_int, Nobs_bnd = int((Nobs - 2)**self.d), 2 * self.d * (Nobs - 2)**(self.d - 1)
+        obs_int, obs_bnd = sample_cube_obs(self.D, Nobs_int, Nobs_bnd, method=method, rng=self.key)
+        return obs_int, obs_bnd
 
     def sample_param(self, Ntarget):
         """
